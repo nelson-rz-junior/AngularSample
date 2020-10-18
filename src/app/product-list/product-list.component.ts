3518b09e43a0fb0214f27afa48bb2d5470daa638
Nelson Roza Junior <jr.1403@hotmail.com>
2020-10-18 14:36:11 -0300

    Initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  public products = products;
+
+  constructor() { }
+
+  share(): void {
+    window.alert('The product has been shared');
+  }
+
+  onNotify(): void {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/products.ts
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    name: 'iPhone X',
+    price: 299,
+    description: 'Super Retina HD display, 5.8-inch (diagonal) all-screen OLED display'
+  },
+  {
+    name: 'iPhone XR',
+    price: 399,
+    description: 'Liquid Retina HD display, 6.1-inch (diagonal) all-screen LCD display with IPS technology'
+  },
+  {
+    name: 'iPhone SE (2nd generation)',
+    price: 499,
+    description: 'Retina HD display, 4.7-inch (diagonal) widescreen LCD display with IPS technology'
+  },
+  {
+    name: 'iPhone 11',
+    price: 599,
+    description: 'Liquid Retina HD display, 6.1-inch (diagonal) all-screen LCD display with IPS technology'
+  },
+  {
+    name: 'iPhone 12 mini',
+    price: 699,
+    description: 'Super Retina XDR display, 5.4-inch (diagonal) all-screen OLED display'
+  },
+  {
+    name: 'iPhone 12',
+    price: 799,
+    description: 'Super Retina XDR display, 6.1-inch (diagonal) all-screen OLED display'
+  },
+  {
+    name: 'iPhone 12 Pro',
+    price: 999,
+    description: 'Super Retina XDR display, 6.1-inch (diagonal) all-screen OLED display'
+  },
+  {
+    name: 'iPhone 12 Pro Max',
+    price: 1099,
+    description: 'Super Retina XDR display, 6.7-inch (diagonal) all-screen OLED display'
+  }
+];
